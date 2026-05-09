6081c7f150feec5a107462263c436934ffd08d11
Greyson Brothers <greysonbrothers@gmail.com>
2026-05-08 20:40:09 -0400

    Add actions

---
+++ filters/actions/accel.py
@@ -0,0 +1,12 @@
+from typing import NamedTuple
+
+
+class LinearAccel2D(NamedTuple):
+    x: float
+    y: float
+    
+class LinearAccel3D(NamedTuple):
+    x: float
+    y: float
+    z: float
+    
